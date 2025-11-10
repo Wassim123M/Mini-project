ef292a02976ef2144678fba95fe859f7ee2fb67a
Wassim123M <162014837+Wassim123M@users.noreply.github.com>
2025-11-10 06:56:11 +0200

    Update HomeController.java

--- Main src/src/main/java/com/simpleapp/miniproject3/HomeController.java
+++ Main src/src/main/java/com/simpleapp/miniproject3/HomeController.java
@@ -10,8 +10,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HomeController {
-
-    // ===== Open Items Management =====
+    
     @FXML
     void openItemsMng(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("item-view.fxml"));
@@ -21,24 +20,21 @@ public class HomeController {
         stage.setMaximized(true);
         stage.show();
     }
-
-    // ===== Open Sports Equipment Management =====
+    
     @FXML
     void openSportsEquipment(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("sport-equipment-view.fxml"));
         Parent root = loader.load();
-
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.setMaximized(true);
         stage.show();
     }
-
-
-    // ===== Exit Application =====
+    
     @FXML
     void onExitApp(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
     }
 }
+
