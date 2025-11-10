4c92d86d1a51338fe861b227e7ed4cc203595795
Wassim123M <162014837+Wassim123M@users.noreply.github.com>
2025-11-10 06:58:21 +0200

    Update SportEquipment.java

--- Main src/src/main/java/com/simpleapp/miniproject3/models/SportEquipment.java
+++ Main src/src/main/java/com/simpleapp/miniproject3/models/SportEquipment.java
@@ -15,24 +15,21 @@ public class SportEquipment {
         this.quantity = new SimpleIntegerProperty(quantity);
         this.price = new SimpleDoubleProperty(price);
     }
-
-    // Name
+    
     public String getName() { return name.get(); }
     public void setName(String name) { this.name.set(name); }
     public StringProperty nameProperty() { return name; }
-
-    // Type
+    
     public String getType() { return type.get(); }
     public void setType(String type) { this.type.set(type); }
     public StringProperty typeProperty() { return type; }
-
-    // Quantity
+    
     public int getQuantity() { return quantity.get(); }
     public void setQuantity(int quantity) { this.quantity.set(quantity); }
     public IntegerProperty quantityProperty() { return quantity; }
-
-    // Price
+    
     public double getPrice() { return price.get(); }
     public void setPrice(double price) { this.price.set(price); }
     public DoubleProperty priceProperty() { return price; }
 }
+
