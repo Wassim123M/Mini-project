5af51349507658e213e61097daac53422411c8f5
Wassim123M <162014837+Wassim123M@users.noreply.github.com>
2025-11-10 06:59:10 +0200

    Update SportEquipmentStore.java

--- Main src/src/main/java/com/simpleapp/miniproject3/models/SportEquipmentStore.java
+++ Main src/src/main/java/com/simpleapp/miniproject3/models/SportEquipmentStore.java
@@ -8,7 +8,6 @@ public class SportEquipmentStore {
     private static final ObservableList<SportEquipment> equipmentList = FXCollections.observableArrayList();
 
     static {
-        // Preset sample equipment
         equipmentList.addAll(
                 new SportEquipment("Soccer Ball", "Ball", 10, 29.99),
                 new SportEquipment("Tennis Racket", "Racket", 5, 79.99),
@@ -17,19 +16,17 @@ public class SportEquipmentStore {
                 new SportEquipment("Badminton Shuttlecock", "Shuttlecock", 50, 15.00)
         );
     }
-
-    // Get the observable list
+    
     public static ObservableList<SportEquipment> getEquipmentList() {
         return equipmentList;
     }
-
-    // Add new equipment
+    
     public static void addEquipment(SportEquipment equipment) {
         equipmentList.add(equipment);
     }
-
-    // Remove equipment
+    
     public static void removeEquipment(SportEquipment equipment) {
         equipmentList.remove(equipment);
     }
 }
+
